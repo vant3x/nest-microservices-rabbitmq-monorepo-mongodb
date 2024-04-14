813bb9db84aa2c1523430baccba469bfd8600375
alevante <alejandrovelasquezmolina@gmail.com>
2024-04-13 22:42:46 -0500

    feat: se crean los demás microservicios base y el scheme de orders base

---
+++ apps/orders/src/schemas/order.schema.ts
@@ -0,0 +1,22 @@
+import { AbstractDocument } from "@app/common";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema({ versionKey: false })
+export class Order extends AbstractDocument {
+    @Prop()
+    name: string;
+
+    @Prop()
+    price: number;
+
+    @Prop()
+    phoneNumber: string;
+
+    @Prop({
+        required: false,
+        default: 1
+    })
+    quantity: number;
+}
+
+export const orderSchema = SchemaFactory.createForClass(Order);
