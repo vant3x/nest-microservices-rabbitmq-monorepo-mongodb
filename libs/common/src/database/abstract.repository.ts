4531208ce41c07a18a82087fa4294cd21fa6e937
alevante <alejandrovelasquezmolina@gmail.com>
2024-04-14 23:35:49 -0500

    fix: repository error

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -38,7 +38,7 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
       throw new NotFoundException('Document not found.');
     }
 
-    return document;
+    return document as TDocument;
   }
 
   async findOneAndUpdate(
